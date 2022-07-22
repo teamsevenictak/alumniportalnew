16d6c5eda046b561fc144d4ab941df6a0d2b808b
teamsevenictak <109238460+teamsevenictak@users.noreply.github.com>
2022-07-22 22:32:35 +0530

    Add files via upload

---
+++ applynow/applynow.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ApplynowComponent } from './applynow.component';
+
+describe('ApplynowComponent', () => {
+  let component: ApplynowComponent;
+  let fixture: ComponentFixture<ApplynowComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ApplynowComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ApplynowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
